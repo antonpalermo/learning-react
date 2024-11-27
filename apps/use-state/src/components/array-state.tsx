fce595af3aea2dc77c67f4e933693aae9d3481db
Anton Palermo <anton.palermo+github@dragonpay.ph>
2024-11-27 23:20:42 +0800

    [DAY: 3] mastering useState
    - fix: updating of array objects
    
    areas of improvement
    - learn more about arrays and objects
    - make updating of array especially array objects less code

--- apps/use-state/src/components/array-state.tsx
+++ apps/use-state/src/components/array-state.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function Task(props: {
-  task: { id: number; name: string; isComplete: boolean }
-  onComplete: (id: number, status: boolean) => void
-}) {
+type Task = {
+  id: number
+  name: string
+  isComplete: boolean
+}
+
+function Task(props: { task: Task; onComplete: (task: Task) => void }) {
   return (
     <div>
       <input
@@ -11,7 +14,9 @@ function Task(props: {
         type='checkbox'
         name='isComplete'
         checked={props.task.isComplete}
-        onChange={e => props.onComplete(props.task.id, e.target.checked)}
+        onChange={e =>
+          props.onComplete({ ...props.task, isComplete: !e.target.checked })
+        }
       />
       {props.task.name}
     </div>
@@ -22,18 +27,29 @@ export default function ArrayState() {
   const [tasks, setTasks] = React.useState([
     { id: 0, name: 'Buy apples', isComplete: false },
     { id: 1, name: 'Buy bananas', isComplete: true },
-    { id: 2, name: 'Buy oranges', isComplete: false }
+    { id: 2, name: 'Buy oranges', isComplete: false },
+    { id: 3, name: 'Buy grapes', isComplete: true },
+    { id: 5, name: 'Buy kiwi', isComplete: true },
+    { id: 6, name: 'Buy cherry', isComplete: true }
   ])
 
-  function handleOnTaskComplete(id: number, complete: boolean) {
-    const taskIndex = tasks.findIndex(e => e.id === id)
-    const updatedTasks = tasks.splice(taskIndex, 1, {
-      id,
-      name: '',
-      isComplete: complete
-    })
+  function changeTaskStatus(id: number, current: boolean) {
+    const currentIndex = tasks.findIndex(task => task.id === id)
+    const task = tasks[currentIndex]
+    if (currentIndex !== -1) {
+      return { ...task, isComplete: (task.isComplete = !current) }
+    }
+    return null
+  }
 
-    setTasks(updatedTasks)
+  function handleOnTaskComplete(task: Task) {
+    const updatedTask = changeTaskStatus(task.id, task.isComplete)
+
+    if (!updatedTask) {
+      return console.log('no avail task')
+    }
+
+    setTasks(prev => prev.map(t => (t.id === task.id ? updatedTask : t)))
   }
 
   return (
