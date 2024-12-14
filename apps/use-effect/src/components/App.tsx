3c8dcd3c385fe0c177a7b5c5d8e829bea1510101
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-12-14 23:27:29 +0800

    [DAY: 6] mastering useEffect

--- apps/use-effect/src/components/App.tsx
+++ apps/use-effect/src/components/App.tsx
@@ -1,3 +1,17 @@
+import React from 'react'
+
 export default function App() {
-  return <h1>App</h1>
+  const [coordinates, setCoordinates] = React.useState()
+
+  React.useEffect(() => {
+    window.addEventListener('mousemove', ev => console.log(ev))
+    return () =>
+      window.removeEventListener('mousemove', () => console.log('removed'))
+  }, [])
+
+  return (
+    <div>
+      <p>{JSON.stringify(coordinates)}</p>
+    </div>
+  )
 }
